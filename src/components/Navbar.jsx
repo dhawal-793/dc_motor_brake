d30f7817c760422e9860eedfc70e0ba87189d379
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-07-12 08:55:33 +0530

    Added functionality and contents for the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,102 @@
+import { useState,useEffect } from "react";
+import { Link,useLocation } from "react-router-dom";
+
+import Container from "./ui/Container";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const location = useLocation();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsNavOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    setIsNavOpen(false);
+  }, [location.pathname]);
+
+  return (
+    <header className="relative w-full">
+      <nav className=" w-full fixed top-0 inset-x-0 py-2 backdrop-blur-sm bg-white/30">
+        <Container>
+          <div className="flex items-center gap-10 justify-between lg:justify-normal">
+            <Link to="/" className="font-bold text-4xl block">
+              Logo
+            </Link>
+            <ul className="hidden lg:flex items-center justify-center gap-6 list-none text-lg font-semibold">
+              {navLinks.map(({ name, link }) => (
+                <li key={name}>
+                  <Link
+                    to={link}
+                    className="hover:text-red-300 duration-300 transition-all"
+                  >
+                    {name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <button className="lg:hidden z-[100]" onClick={toggleMenu}>
+              <span
+                className={`fa-solid ${
+                  !isNavOpen ? "fa-bars -rotate-180" : "fa-xmark rotate-180"
+                } transition-all text-3xl `}
+              />
+            </button>
+          </div>
+        </Container>
+      </nav>
+      <nav
+        className={`lg:hidden ${
+          isNavOpen ? "translate-x-0 " : "-translate-x-full"
+        } transition-all duration-500  min-h-screen w-[75vw] max-w-[420px] bg-[#5B1892] fixed left-0 top-0 z-50`}
+      >
+        <div className="py-3">
+          <Link
+            to="/"
+            className="block text-4xl text-white font-bold text-center"
+          >
+            LOGO
+          </Link>
+        </div>
+        <ul className=" space-y-6 list-none text-lg font-semibold p-8">
+          {navLinks.map(({ name, link }) => (
+            <li key={name}>
+              <Link
+                to={link}
+                className="hover:text-red-300 duration-300 transition-all"
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="h-16"/>
+    </header>
+  );
+};
+
+export default Navbar;
+
+const navLinks = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Applications",
+    link: "/applications",
+  },
+  {
+    name: "Products",
+    link: "/products",
+  },
+  {
+    name: "About Us",
+    link: "/about",
+  },
+  {
+    name: "Contact Us",
+    link: "/contact",
+  },
+];
