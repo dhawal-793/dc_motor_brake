92cd1163f0bb0b86598c880a706685bbe5ad2626
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-07-13 17:43:37 +0530

    Updated Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="py-3 w-full text-lg border-t-2">
+        <p className="mx-auto w-fit">
+          Copyright &copy; All Rights Reserved | Unique MotorBrakes
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
