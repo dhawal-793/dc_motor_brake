810154836fa1b42be4654eafbb7a6626c09c4c5a
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-07-14 11:37:06 +0530

    Added animation using AnimatedWrapper.

--- src/components/home-components/OurClients.jsx
+++ src/components/home-components/OurClients.jsx
@@ -1,4 +1,7 @@
+import AnimatedWrapper from "../ui/AnimatedWrapper";
 import Container from "../ui/Container";
+
+// import { clients } from "../../data/our-clients";
 
 const OurClients = () => {
   return (
@@ -7,10 +10,9 @@ const OurClients = () => {
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8 place-items-center max-w-screen-xl mx-auto my-8">
         {[...Array(30)].map((_, i) => {
           return (
-            <div
-              key={i}
-              className="size-20 md:size-28 lg:size-32 p-2 bg-green-300 mx-auto"
-            />
+            <AnimatedWrapper animateFrom="right" key={i}>
+              <div className="size-20 md:size-28 lg:size-32 p-2 bg-green-300 mx-auto" />
+            </AnimatedWrapper>
           );
         })}
       </div>
